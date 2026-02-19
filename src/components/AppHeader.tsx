1eb18a99b34f74f6af72d3c3a7ebf7017b652bd8
Alec <alec@antsa.com.au>
2026-02-19 17:47:37 +1100

    Prevent navbar items from collapsing into overflow menu on desktop
    Add disabledOverflow to Ant Design Menu to stop auto-collapsing
    nav items into a three-dot dropdown when there is enough space.

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -97,6 +97,7 @@ const AppHeader = () => {
         >
           <Menu
             mode="horizontal"
+            disabledOverflow
             items={menuItems}
             style={{
               border: 'none',
